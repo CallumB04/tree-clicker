2cbde3f6119cecc8e05cddd2e798decd8f503e92
Cal <callumburgoyne04@gmail.com>
2025-07-09 01:14:40 +0100

    feat: add basic scoring system

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,7 +1,20 @@
 extends Control
 
+var trees: int = 0; ## game score
+var current_tree_progress: int = 0; ## value 0-100, when 100 plants tree
+var tree_click_strength: int = 30; ## tree progress per click
+var tree_progress_per_second: int = 0; ## automatic tree progress per second
+
 func _ready() -> void:
-	$TreeClicker.connect("pressed", _on_tree_clicked)
+	$TreeClicker.connect("pressed", _on_tree_clicked);
 	
 func _on_tree_clicked():
-	print("tree clicked")
+	## adding progress to tree
+	current_tree_progress += tree_click_strength;
+	print("Tree Progress: ", current_tree_progress);
+	
+	## increasing score and resetting tree once complete
+	if current_tree_progress >= 100:
+		current_tree_progress -= 100;
+		trees += 1;
+		print("Tree planted. Trees: ", trees)
