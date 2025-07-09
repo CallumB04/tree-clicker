19f5b387544a1cf1bf9b02d77c5f79df087c9d5d
Cal <callumburgoyne04@gmail.com>
2025-07-09 21:59:37 +0100

    feat: improve UI layout

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -2,7 +2,7 @@ extends Control
 
 var trees: int = 0; ## game score
 var current_tree_progress: int = 0; ## value 0-100, when 100 plants tree
-var tree_click_strength: int = 1; ## tree progress per click
+var tree_click_strength: int = 5; ## tree progress per click
 var tree_progress_per_second: int = 0; ## automatic tree progress per second
 
 func _ready() -> void:
@@ -18,15 +18,15 @@ func _on_tree_clicked():
 		trees += 1;
 		$ScoreContainer/TreeCount.text = "Trees: %s" % [trees]; ## updating trees score text
 		$TreeClicker.texture_normal = preload("res://Assets/Trees/Oak1.png");
-		$TreeClicker.set_position(Vector2(209, 420));
+		$TreeClicker.pivot_offset = Vector2(8, 13);
 	elif current_tree_progress >= 75:
 		$TreeClicker.texture_normal = preload("res://Assets/Trees/Oak4.png");
-		$TreeClicker.set_position(Vector2(135, 222));
+		$TreeClicker.pivot_offset = Vector2(23, 63);
 	elif current_tree_progress >= 50:
 		$TreeClicker.texture_normal = preload("res://Assets/Trees/Oak3.png");
-		$TreeClicker.set_position(Vector2(185, 348));
+		$TreeClicker.pivot_offset = Vector2(12, 32);
 	elif current_tree_progress >= 25:
 		$TreeClicker.texture_normal = preload("res://Assets/Trees/Oak2.png");
-		$TreeClicker.set_position(Vector2(206, 393));
+		$TreeClicker.pivot_offset = Vector2(8, 19);
 		
 	$ScoreContainer/ProgressContainer/TreeProgress.value = current_tree_progress; ## updating tree progress bar
